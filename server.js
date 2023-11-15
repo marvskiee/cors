e07e3992893e4b2456cac776bea2bf54028b276f
Marvi Lean Negrete <acemonkey2@gmail.com>
2023-11-15 10:20:22 +0800

    first commit

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
 
-const app = express();
+// Use dynamic import to import node-fetch
+import('node-fetch').then(({ default: fetch }) => {
+  const app = express();
 
-// Enable CORS for all routes
-app.use(cors());
+  // Enable CORS for all routes
+  app.use(cors());
 
-// Define a route to proxy the requests
-app.get('/proxy', async (req, res) => {
-  const { url } = req.query;
+  // Define a route to proxy the requests
+  app.get('/proxy', async (req, res) => {
+    const { url } = req.query;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.blob();
+    try {
+      const response = await fetch(url);
+      const data = await response.blob();
 
-    res.setHeader('Content-Type', response.headers.get('Content-Type'));
-    res.send(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).send('Error fetching data');
-  }
-});
+      res.setHeader('Content-Type', response.headers.get('Content-Type'));
+      res.send(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).send('Error fetching data');
+    }
+  });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}).catch(error => {
+  console.error('Error importing node-fetch:', error);
 });
