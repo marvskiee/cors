09ffd514e59d144a1efec2a80f96f1df11578c1e
Marvi Lean Negrete <acemonkey2@gmail.com>
2023-11-15 10:14:35 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Define a route to proxy the requests
+app.get('/proxy', async (req, res) => {
+  const { url } = req.query;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    res.setHeader('Content-Type', response.headers.get('Content-Type'));
+    res.send(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Error fetching data');
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
